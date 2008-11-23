4d214304355e65b3198f9280ec45df6eb2afcf59
mcculls <mcculls@0796d716-ab03-11dd-a887-7ffe1a420f8d>
2008-11-23 13:50:57 +0000

    Refactored common launcher code into separate artifact

--- trunk/chapter02/paint-nonmodular/STANDALONE.bat
+++ trunk/chapter02/paint-nonmodular/STANDALONE.bat
@@ -1,16 +1,12 @@
 @ECHO OFF
 
 SET _EXAMPLE_DIR_=%~dp0
-SET _EXAMPLE_BSN_=org.foo.paint
-SET _EXAMPLE_VER_=1.0
-
-SET _EXAMPLE_JAR_=%_EXAMPLE_BSN_%-%_EXAMPLE_VER_%.jar
 
 pushd %_EXAMPLE_DIR_%
 
-IF NOT EXIST target/%_EXAMPLE_JAR_% CALL mvn install
+IF NOT EXIST target/paint.jar CALL mvn install
 
-START javaw -jar target/%_EXAMPLE_JAR_%
+START javaw -jar target/paint.jar
 
 popd
 
