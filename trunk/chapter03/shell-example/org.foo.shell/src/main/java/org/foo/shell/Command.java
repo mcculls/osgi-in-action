f31fecae0a895598a09c2a604905eb98e7a2b1bc
mcculls <mcculls@0796d716-ab03-11dd-a887-7ffe1a420f8d>
2009-02-25 09:36:33 +0000

    Remove old build files, will be replaced by new build system

--- trunk/chapter03/shell-example/org.foo.shell/src/main/java/org/foo/shell/Command.java
+++
@@ -1,7 +0,0 @@
-package org.foo.shell;
-
-import java.io.PrintStream;
-
-public interface Command {
-  public void exec(String args, PrintStream out, PrintStream err) throws Exception;
-}
--- trunk/chapter03/shell-example/org.foo.shell/src/main/java/org/foo/shell/History.java
+++
@@ -1,7 +0,0 @@
-package org.foo.shell;
-
-import java.util.List;
-
-public interface History {
-  public List<String> get();
-}
